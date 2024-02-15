32fddd650410a2f61c1bb520cf97004bada5ff3f
lx779940 <3122178991@qq.com>
2024-02-15 23:48:33 +0800

    feat:Joi验证

---
+++ server/limit/login.js
@@ -0,0 +1,18 @@
+const joi = require("joi");
+//账号的验证
+const account = joi.string().alphanum().min(6).max(12).required();
+//密码的验证
+const password = joi
+  .string()
+  .pattern(/^(?![0-9]+$)[a-z0-9]{1,50}$/)
+  .min(6)
+  .max(12)
+  .required();
+
+exports.login_limit = {
+  //对body的验证
+  body: {
+    account: account,
+    password: password,
+  },
+};
