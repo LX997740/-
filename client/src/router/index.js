05a870de5b3651a16babef54ad0a975e7d32fe7c
lx779940 <3122178991@qq.com>
2024-02-16 22:16:10 +0800

    feat:新增路由

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,32 @@
-import {
-	createRouter,
-	createWebHashHistory
-} from 'vue-router'
+import { createRouter, createWebHashHistory } from "vue-router";
 
-const routes = [{}]
+const routes = [
+  {
+    path: "/",
+    redirect: "/login",
+  },
+
+  {
+    name: "login",
+    path: "/login",
+    component: () => import("@/views/login/Login.vue"),
+  },
+  {
+    name: "menu",
+    path: "/menu",
+    component: () => import("@/views/menu/Menu.vue"),
+    children: [
+      {
+        name: "home",
+        path: "/home",
+        component: () => import("@/views/home/Home.vue"),
+      },
+    ],
+  },
+];
 
 const router = createRouter({
-	history: createWebHashHistory(),
-	routes
-})
-export default router
+  history: createWebHashHistory(),
+  routes,
+});
+export default router;
